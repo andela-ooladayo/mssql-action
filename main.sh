8ba0ebc998456cccb5b27a678385faa6b1d19cce
Oladayo Oyelade <ayfrank2@gmail.com>
2020-12-17 14:53:35 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker_run="docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$INPUT_PASSWORD' -p $INPUT_PORT:1433 -d mcr.microsoft.com/mssql/server:$INPUT_VERSION"
+docker_run="docker run -e 'ACCEPT_EULA=Y' -p $INPUT_PORT:1433 -d mcr.microsoft.com/mssql/server:$INPUT_VERSION"
 
 echo "RUNNING: $docker_run"
 sh -c "$docker_run"
